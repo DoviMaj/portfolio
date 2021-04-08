da4e62ed9db4e5d04b17958caa0c979062e3f558
dovimaj <dovimajowka@gmail.com>
2021-04-08 03:44:05 +0300

    Separate projects into own file

---
+++ projects.js
@@ -0,0 +1,69 @@
+const projects = [
+  {
+    title: "Fakebook",
+    live_url: "https://fakebook-client.vercel.app/",
+    code_url: "https://github.com/DoviMaj/fakebook-client",
+    description:
+      "Make friends, chat with them in real time, post text and images, authenticate with Facebook or Google.",
+    tech: [
+      "react",
+      "reactrouter",
+      "typescript",
+      "sass",
+      "node-dot-js",
+      "express",
+      "mongodb",
+      "socket-dot-io",
+    ],
+  },
+  {
+    title: "Blog Project",
+    live_url: "https://blog-client-dovimaj.vercel.app/",
+    code_url: "https://github.com/DoviMaj/blog-client",
+    description:
+      "Owners can edit posts when authenticated, users can comment, data is synced with MongoDB through REST API.",
+    tech: [
+      "next-dot-js",
+      "typescript",
+      "bootstrap",
+      "sass",
+      "express",
+      "mongodb",
+    ],
+  },
+
+  {
+    title: "Note App",
+    live_url: "https://dovimaj.github.io/note-app-2.0/",
+    code_url: "https://github.com/DoviMaj/note-app-2.0",
+    description:
+      "Take notes, add tasks to your notes, change the background image and sync with your google account.",
+    tech: ["react", "firebase", "google"],
+  },
+  {
+    title: "Current Weather",
+    live_url: "https://dovimaj.github.io/weather-app/",
+    code_url: "https://github.com/DoviMaj/weather-app",
+    description:
+      "Made to practice API calls, promises with async await. openweathermap and Geolocation API.",
+    tech: ["react"],
+  },
+  {
+    title: "Battleship Game",
+    live_url: "https://dovimaj.github.io/battleship/",
+    code_url: "https://github.com/DoviMaj/battleship",
+    description:
+      "Made to practice TDD (Test Driven Development) principles with Jest and VanillaJS.",
+    tech: ["javascript", "jest"],
+  },
+  {
+    title: "Shopping-cart",
+    live_url: "https://dovimaj.github.io/shopping-cart/",
+    code_url: "https://github.com/DoviMaj/shopping-cart",
+    description:
+      "Made to practice React Functional Components, Hooks and React Router.",
+    tech: ["react", "reactrouter", "bootstrap"],
+  },
+];
+
+export default projects;
