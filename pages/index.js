e3d6fc2a5907452d362da10ea01cd6ac2078e147
dovimaj <dovimajowka@gmail.com>
2021-04-08 23:52:04 +0300

    Add github and linkedin icons

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import ProjectCard from "../components/ProjectCard";
 import projects from "../projects";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Hi 👋, I'm{" "}
-          <a target="_blank" href="https://github.com/DoviMaj">
+          <a rel="noopener" target="_blank" href="https://github.com/DoviMaj">
             David Majowka!
           </a>
         </h1>
@@ -22,11 +23,27 @@ export default function Home() {
           A passionate self-taught full-stack web developer, <br></br>I love
           learning new technologies, building and improving UI's.<br></br>{" "}
           Regular contributor to{" "}
-          <a href="https://github.com/TheOdinProject/" target="_blank">
+          <a
+            rel="noopener"
+            href="https://github.com/TheOdinProject/"
+            target="_blank"
+          >
             TheOdinProject
           </a>
           .
-        </p>{" "}
+        </p>
+        <div className={styles.social}>
+          <a rel="noopener" href="https://github.com/dovimaj/" target="_blank">
+            <Image src="/github.svg" alt="me" width="34" height="34"></Image>
+          </a>
+          <a
+            rel="noopener"
+            href="https://linkedin.com/in/dovid-majowka-8472a0126"
+            target="_blank"
+          >
+            <Image src="/linkedin.svg" alt="me" width="34" height="34"></Image>
+          </a>
+        </div>
         <code className={styles.code}>Here are some of my projects:</code>
         <div className={styles.grid}>
           {projects.map(
@@ -47,6 +64,7 @@ export default function Home() {
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          rel="noopener"
           target="_blank"
           rel="noopener noreferrer"
         >
